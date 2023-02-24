2b50e5802a020f14833a98976ae13379332e42dc
M6GSN <48696854+M6GSN@users.noreply.github.com>
2023-02-24 14:29:03 +0000

    Create install.sh
    first copy

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt update
+apt install curl gpg
+cd /tmp
+wget http://apt.allstarlink.org/repos/asl_builds/install-allstarlink-repository
+chmod +x install-allstarlink-repository
+./install-allstarlink-repository
+apt -y install linux-headers-$(uname -r)
+asl-asterisk allstar-helpers asl-dahdi-linux asl-update-node-list
