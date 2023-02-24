4d81885b6986ca175ad587e69577aecc44237a29
M6GSN <48696854+M6GSN@users.noreply.github.com>
2023-02-24 16:36:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ cd /tmp
 wget http://apt.allstarlink.org/repos/asl_builds/install-allstarlink-repository
 chmod +x install-allstarlink-repository
 ./install-allstarlink-repository
-apt -y install linux-headers-$(uname -r)
-asl-asterisk allstar-helpers asl-dahdi-linux asl-update-node-list
+apt -y install linux-headers-$(uname -r) asl-asterisk allstar-helpers asl-dahdi-linux asl-update-node-list
