f33715bf73381fcb813441bf2f8e3589744ba66a
M6GSN <48696854+M6GSN@users.noreply.github.com>
2023-02-27 22:05:29 +0000

    Update allmon.sh

---
+++ allmon.sh
@@ -0,0 +1,8 @@
+apt update
+apt install apache2 php libapache2-mod-php
+git clone https://github.com/AllStarLink/AllMon2.git /var/www/html/
+cd /var/www/html/
+mv allmon.ini.txt allmon.ini.php
+mv controlpanel.ini.txt controlpanel.ini.php
+touch astdb.txt
+chmod 777 astdb.txt
