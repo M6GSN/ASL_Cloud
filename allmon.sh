d4b9a72e731f4938cb228e27fb16140949d89264
M6GSN <48696854+M6GSN@users.noreply.github.com>
2023-02-28 13:25:20 +0000

    Update allmon.sh

--- allmon.sh
+++ allmon.sh
@@ -1,5 +1,5 @@
 apt update
-apt install apache2 php libapache2-mod-php
+apt install apache2 php libapache2-mod-php -y
 git clone https://github.com/AllStarLink/AllMon2.git /var/www/html/
 cd /var/www/html/
 mv allmon.ini.txt allmon.ini.php
